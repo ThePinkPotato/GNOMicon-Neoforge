22b276f92524578a9f941b78181cb8e6f49ceaca
Haxalotl <haxalotlsjunk@gmail.com>
2026-09-05 23:38:35 -0400

    Remove unneeded code and comments from template generation

--- src/main/java/dev/haxalotl/gnomicon/GNOMicon.java
+++ src/main/java/dev/haxalotl/gnomicon/GNOMicon.java
@@ -1,29 +1,20 @@
 package dev.haxalotl.gnomicon;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.loading.FMLPaths;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.slf4j.Logger;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(GNOMicon.MODID)
 public class GNOMicon {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "gnomicon";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public static void createDesktopEntry() {
@@ -54,30 +45,11 @@ public class GNOMicon {
             e.printStackTrace();
         }
     }
-
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public GNOMicon(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-
-
-
+            LOGGER.info("GNOME is gonna give me an aneurysm");
         }
     }
 
